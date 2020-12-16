8118d37ff92a2375395ee3b9de50681969fcb39f
Emil Lerner <neex.emil@gmail.com>
2020-12-16 18:19:43 +0300

    add forgotten files

---
+++ go.mod
@@ -0,0 +1,9 @@
+module mydns
+
+go 1.12
+
+require (
+	github.com/miekg/dns v1.1.16
+	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
+	github.com/shurcooL/vfsgen v0.0.0-20181202132449-6a9ea43bcacd // indirect
+)
---
+++ make_docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf html-docs && mkdir html-docs && ( cat <<EOF && pandoc  --from commonmark --to html  -i README.md && echo '</body></html>' ) > html-docs/index.html
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Style-Type" content="text/css" />
+  <meta name="generator" content="pandoc" />
+  <title>1u.ms</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css" type="text/css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js" integrity="sha256-1zu+3BnLYV9LdiY85uXMzii3bdrkelyp37e0ZyTAQh0=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/languages/shell.min.js" integrity="sha256-nwOM3xEc6CFfrPNDN1upX+5ynjWKAXsg+bW63SSzte0=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/default.min.css" integrity="sha256-zcunqSn1llgADaIPFyzrQ8USIjX2VpuxHzUwYisOwo8=" crossorigin="anonymous" /> 
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body class="markdown-body">
+EOF
